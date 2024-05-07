c6d62477210ea5c7e05ab6c67c0bc82a03e99731
vamshi-vcd <113637912+vamshi-vcd@users.noreply.github.com>
2024-05-07 20:50:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,56 +1,92 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+import pyautogui
 import streamlit as st
-import subprocess
-import io
 
-# Function to execute Python script and capture output
-def execute_script(script_file):
-    try:
-        # Execute the Python script
-        process = subprocess.Popen(['python', script_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        output, error = process.communicate()
+# Function to calculate angle
+def calculate_angle(a, b, c):
+    a = np.array(a)
+    b = np.array(b)
+    c = np.array(c)
+    
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = np.abs(radians*180.0/np.pi)
+    
+    if angle > 180.0:
+        angle = 360 - angle
         
-        # Check for errors
-        if error:
-            return f"Error occurred: {error.decode('utf-8')}"
+    return angle
+
+# Initialize mediapipe instances
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+# Capture video
+cap = cv2.VideoCapture(0)
+
+# Curl counter variables
+counter = 0 
+stage = None
+
+# Setup mediapipe instance
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    st.title("Fitness App")
+    st.write("Pose Estimation and Virtual Mouse Control")
+
+    while cap.isOpened():
+        ret, frame = cap.read()
         
-        return output.decode('utf-8')
-    except Exception as e:
-        return f"Error occurred: {e}"
+        # Recolor image to RGB
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+      
+        # Make detection
+        results = pose.process(image)
+    
+        # Recolor back to BGR
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+        # Extract landmarks
+        try:
+            landmarks = results.pose_landmarks.landmark
+            
+            # Get coordinates
+            shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+            elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+            wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+            
+            # Calculate angle
+            angle = calculate_angle(shoulder, elbow, wrist)
+            
+            # Visualize angle
+            cv2.putText(image, str(angle), 
+                           tuple(np.multiply(elbow, [640, 480]).astype(int)), 
+                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                                )
+            
+            # Curl counter logic
+            if angle > 160:
+                stage = "down"
+            if angle < 30 and stage =='down':
+                stage="up"
+                counter +=1
+                st.write("Reps: ", counter)
+                       
+        except:
+            pass
+        
+        # Render detections
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
+                                 )               
+        
+        st.image(image, channels="BGR")
 
-# Streamlit app
-def main():
-    st.title('MULTIMODAL FUSION TO ENHANCE HUMAN COMPUTER INTERACTION')
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
 
-    # File paths of Python script files
-    curlcounter_file="curlcounter.py"
-    eyetracker_file = "eyetracker.py"
-    handtracker_file = "handtracker.py"
-
-    # Align buttons at the center
-    st.write("<div style='text-align: center;'>", unsafe_allow_html=True)
-
-    st.markdown("<h3><b>Run Curl Counter</b></h3>", unsafe_allow_html=True)
-    if st.button('Click here to open Curl Counter', key="curncounter"):
-        st.write("### Opening Curl Counter")
-        output_curlcounter = execute_script(curlcounter_file)
-        st.code(output_curlcounter, language='python')
-
-    # Button to execute eye_tracker.py
-    st.markdown("<h3><b>Run Eye Tracker</b></h3>", unsafe_allow_html=True)
-    if st.button('Click here to open Eye Tracking Mouse', key="eyetracker"):
-        st.write("### Opening Eye Tracker")
-        output_eyetracker = execute_script(eyetracker_file)
-        st.code(output_eyetracker, language='python')
-
-    # Button to execute hand_tracker.py
-    st.markdown("<h3><b>Run Hand Tracker</b></h3>", unsafe_allow_html=True)
-    if st.button('Click here to open Hand Tracking Mouse', key="handtracker"):
-        st.write("### Opening Hand Tracker")
-        output_handtracker = execute_script(handtracker_file)
-        st.code(output_handtracker, language='python')
-
-    st.write("</div>", unsafe_allow_html=True)
-
-# Run the Streamlit app
-if __name__ == "__main__":
-    main()
+    cap.release()
+    cv2.destroyAllWindows()
